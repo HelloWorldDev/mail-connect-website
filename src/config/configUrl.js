c48d88f1105157fdc1ad58d15aa731267cfa7ff5
dengzeyuan <dengzeyuan@nihao.com>
2022-06-29 14:52:44 +0800

    添加不同环境标志

--- src/config/configUrl.js
+++ src/config/configUrl.js
@@ -3,18 +3,22 @@ const modeUrlObj = {
     // 正式环境
     'prod': {
       base_url: 'https://prod1:9991',  
+      node_env:'prod'
     },
     //测试环境
     'test': {
       base_url: 'http://test1:9991', 
+      node_env:'test'
     },
     //灰度环境
     'pre': {
       base_url: 'http://pre1:9991', 
+      node_env:'pre'
     },
     // 本地环境
     'dev': {
       base_url: 'http://dev1:9991', 
+      node_env:'dev'
     },
 }
 export default modeUrlObj[process.env.VUE_APP_TITLE]
